b4ebae214a4efe28297b20eec15f399164160903
LAPTOP-R5J8GTP0\alessandro <alessandro.pasqua02@gmail.com>
2020-11-28 00:40:53 +0100

    settato mossa del pedone

--- src/main/java/it/unibas/scacchi/modello/AbstractPezzo.java
+++ src/main/java/it/unibas/scacchi/modello/AbstractPezzo.java
@@ -6,6 +6,7 @@ import java.util.List;
 public abstract class AbstractPezzo implements Pezzo{
     
     private List<Mossa> mossePossibili = new ArrayList<Mossa>();
+    
     private String colore ;
     private int posX;
     private int posY;
@@ -17,6 +18,18 @@ public abstract class AbstractPezzo implements Pezzo{
     }
     
     //Metodi Di ogni Pezzo
+    
+    public List<Mossa> filtraListaFuoriScacchiera(){
+        for (Mossa mossa : this.mossePossibili) {
+            if(mossa.getSuccX() > 7 || mossa.getSuccX() < 0){
+                this.mossePossibili.remove(mossa);
+            }
+            if(mossa.getSuccY() > 7 || mossa.getSuccY() < 0){
+                this.mossePossibili.remove(mossa);
+            }
+        }
+        return mossePossibili;
+    }
     
     public void cambiaPosizione(int x , int y ){
         this.posX = x;
@@ -53,4 +66,7 @@ public abstract class AbstractPezzo implements Pezzo{
         return mossePossibili;
     }
     
+    public void addMossa(Mossa m) {
+        this.mossePossibili.add(m);
+    }
 }
