90943b9ec8b98fa495fff136022f1994420c3f8b
RoccoCortese <roccocortese14@gmail.com>
2020-12-05 16:49:00 +0100

    aggiunto test.

--- src/test/java/it/unibas/scacchi/test/TestDAOScacchieraJson.java
+++ src/test/java/it/unibas/scacchi/test/TestDAOScacchieraJson.java
@@ -45,11 +45,32 @@ public class TestDAOScacchieraJson extends TestCase{
     public void testModificaSalvaECaricaScacchiera() {
         try {
             Scacchiera scacchiera = dao.carica("./Scacchiera.json");
-            Pezzo torreNera = scacchiera.getPezzo(0, 0);
-            scacchiera.movimentoPezzo(torreNera, new Mossa(0, 0, 4, 4));
+            Pezzo pedoneNero1 = scacchiera.getPezzo(1, 0);
+            Pezzo pedoneNero2 = scacchiera.getPezzo(1, 1);
+            Pezzo pedoneNero3 = scacchiera.getPezzo(1, 2);
+            Pezzo pedoneNero4 = scacchiera.getPezzo(1, 4);
+            Pezzo pedoneNero5 = scacchiera.getPezzo(1, 5);
+            
+            Pezzo pedoneBianco1 = scacchiera.getPezzo(6, 1);
+            Pezzo pedoneBianco2 = scacchiera.getPezzo(6, 2);
+            Pezzo pedoneBianco3 = scacchiera.getPezzo(6, 3);
+            Pezzo pedoneBianco4 = scacchiera.getPezzo(6, 4);
+            Pezzo pedoneBianco5 = scacchiera.getPezzo(6, 5);
+            
+            scacchiera.movimentoPezzo(pedoneNero1, new Mossa(1, 0, 2, 0));
+            scacchiera.movimentoPezzo(pedoneNero2, new Mossa(1, 1, 2, 1));
+            scacchiera.movimentoPezzo(pedoneNero3, new Mossa(1, 2, 2, 2));
+            scacchiera.movimentoPezzo(pedoneNero4, new Mossa(1, 4, 2, 4));
+            scacchiera.movimentoPezzo(pedoneNero5, new Mossa(1, 5, 2, 5));
+            
+            scacchiera.movimentoPezzo(pedoneBianco1, new Mossa(6, 1, 5, 1));
+            scacchiera.movimentoPezzo(pedoneBianco2, new Mossa(6, 2, 5, 2));
+            scacchiera.movimentoPezzo(pedoneBianco3, new Mossa(6, 3, 5, 3));
+            scacchiera.movimentoPezzo(pedoneBianco4, new Mossa(6, 4, 5, 4));
+            scacchiera.movimentoPezzo(pedoneBianco5, new Mossa(6, 5, 5, 5));
             dao.salva(scacchiera, "./Scacchiera.json");
             Scacchiera scacchieraDopo = dao.carica("./Scacchiera.json");
-            assertEquals(scacchieraDopo.getPezzo(4, 4).getColore(), Costanti.NERO);
+            assertEquals(scacchieraDopo.getPezzo(2, 1).getColore(), Costanti.NERO);
         } catch (DAOException ex) {
             logger.debug("Errore nel caricamento della scacchiera" + ex.getMessage());
             fail();
